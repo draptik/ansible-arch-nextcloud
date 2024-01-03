e1e1d55254b6c43e55132fbf21c1e8e081ae75ef
Patrick Drechsler <patrick@pdrechsler.de>
2024-01-03 20:28:31 +0100

    💡 Ansible playbook: add comment how to add verbosity

--- run-playbook-nextcloud.sh
+++ run-playbook-nextcloud.sh
@@ -3,6 +3,7 @@
 set -o nounset
 set -o errexit
 
+## Note: append `-vvv` to the end of the command for verbose output
 ansible-playbook \
     --vault-password-file=.vault_pass \
     --inventory ansible-inventory.cfg \
